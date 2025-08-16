38409e05fec5bc69a971546705211bb47af50689
Ann K <kubareva.ann@gmail.com>
2025-08-16 11:13:18 +0300

    added meta

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,5 +1,6 @@
 import { fetchNotes } from "@/lib/api";
 import NotesClient from "./Notes.client";
+import { Metadata } from "next";
 
 interface NotesPageProps {
   params: Promise<{ slug: string[] }>;
@@ -12,3 +13,23 @@ export default async function NotesPage({ params }: NotesPageProps) {
 
   return <NotesClient initialData={initialData} initialTag={tagNote} />;
 }
+
+// META
+
+export const metadata: Metadata = {
+  title: "Welcome to the notes dashboard",
+  description: "You'll find all your notes here",
+  openGraph: {
+    title: "Welcome to the notes dashboard",
+    description: "You'll find all your notes here",
+    url: "https://notehub.com/notes/action/create",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Notes Dashboard",
+      },
+    ],
+  },
+};
