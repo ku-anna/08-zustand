cc04c6a316f5a32fd65a9da50fee36c8ba61827d
Ann K <kubareva.ann@gmail.com>
2025-08-16 12:02:34 +0300

    fixed notes client create path

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -1,21 +1,20 @@
 "use client";
-import { SearchBox } from "@/components/SearchBox/SearchBox";
-import css from "./NotesPage.module.css";
-import { fetchNotes } from "@/lib/api";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
+
+import { SearchBox } from "@/components/SearchBox/SearchBox";
 import Pagination from "@/components/Pagination/Pagination";
 import { Loader } from "@/components/Loader/Loader";
 import { ErrorMessage } from "@/components/ErrorMessage/ErrorMessage";
 import { ErrorMessageEmpty } from "@/components/ErrorMessageEmpty/ErrorMessageEmpty";
-
 import NoteList from "@/components/NoteList/NoteList";
-import { Modal } from "@/components/Modal/Modal";
-import { NoteForm } from "@/components/NoteForm/NoteForm";
 
-import { useDebounce } from "use-debounce";
-import { Toaster } from "react-hot-toast";
+import { fetchNotes } from "@/lib/api";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+
+import css from "./NotesPage.module.css";
 import { Note } from "@/types/note";
 
 interface NotesClientProps {
@@ -23,20 +22,17 @@ interface NotesClientProps {
     notes: Note[];
     totalPages: number;
   };
-
   initialTag: string;
 }
 
 export default function NotesClient({
   initialData,
-
   initialTag,
 }: NotesClientProps) {
   const [currentPage, setCurrentPage] = useState(1);
   const [query, setQuery] = useState("");
   const [debouncedQuery] = useDebounce(query, 1000);
   const [tag, setTag] = useState(initialTag);
-  const [isOpenModal, setIsOpenModal] = useState(false);
 
   useEffect(() => {
     setTag(initialTag);
@@ -52,20 +48,16 @@ export default function NotesClient({
 
   const totalPages = data?.totalPages ?? 0;
 
-  const handleCreateNote = () => {
-    setIsOpenModal(true);
-  };
-
-  const handleCloseModal = () => setIsOpenModal(false);
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
     setCurrentPage(1);
   };
+
   return (
     <div className={css.app}>
       <div className={css.toolbar}>
         <SearchBox value={query} onChange={handleChange} />
+
         {isSuccess && totalPages > 1 && (
           <Pagination
             page={currentPage}
@@ -73,20 +65,17 @@ export default function NotesClient({
             onChange={setCurrentPage}
           />
         )}
-        <button onClick={handleCreateNote} className={css.button}>
-          Create note +
-        </button>
+
+        <Link href="/notes/action/create">
+          <button className={css.button}>Create note +</button>
+        </Link>
       </div>
+
       {isLoading && <Loader />}
       {isError && <ErrorMessage />}
-      <Toaster position="top-right" />
-      {isSuccess && data?.notes.length === 0 && <ErrorMessageEmpty />}
+
+      {isSuccess && data.notes.length === 0 && <ErrorMessageEmpty />}
       {isSuccess && data.notes.length > 0 && <NoteList notes={data.notes} />}
-      {isOpenModal && (
-        <Modal onClose={handleCloseModal}>
-          <NoteForm onClose={handleCloseModal} />
-        </Modal>
-      )}
     </div>
   );
 }
